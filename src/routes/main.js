e13ab36be4ed2ecb63a5658ede4d8f03e6258847
Soledad Vallejos <soledad_vallejos@hotmail.com>
2021-10-10 20:08:28 -0300

    se agregan rutas

---
+++ src/routes/main.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const{index, contact, about, music} = require('../controllers/mainController')
+
+router
+    .get('/', index)
+    .get('/contact', contact)
+    .get('/about', about)
+    .get('/music', music)
+
+module.exports = router;
