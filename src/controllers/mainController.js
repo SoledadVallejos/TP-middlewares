db79b788432ce79e64a723ac74aab9033ee7af42
Soledad Vallejos <soledad_vallejos@hotmail.com>
2021-10-11 01:16:23 -0300

    middleware por ruta

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,6 +2,8 @@ module.exports = {
     index : (req, res) => res.render('index'),
     contact : (req, res) => res.render('contact'),
     about : (req, res) => res.render('about'),
-    music : (req, res) => res.render('music')
+    music : (req, res) => res.render('music'),
+    admin : (req, res) => res.render('admin'),
+    login : (req, res) => res.render('login')
 
 }
